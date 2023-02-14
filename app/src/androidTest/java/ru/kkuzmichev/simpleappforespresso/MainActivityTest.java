22ca608957c096cce6886277662efe0e753a5d14
Leonid Zhdanov <zhdanov.leon@gmail.com>
2023-02-14 20:57:11 +0400

    create IntentTest

---
+++ app/src/androidTest/java/ru/kkuzmichev/simpleappforespresso/MainActivityTest.java
@@ -0,0 +1,34 @@
+package ru.kkuzmichev.simpleappforespresso;
+
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static androidx.test.espresso.matcher.ViewMatchers.withText;
+
+import androidx.test.espresso.ViewInteraction;
+import androidx.test.ext.junit.rules.ActivityScenarioRule;
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+import androidx.test.filters.LargeTest;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import io.qameta.allure.android.runners.AllureAndroidJUnit4;
+
+@LargeTest
+@RunWith(AllureAndroidJUnit4.class)
+public class MainActivityTest {
+
+    @Rule
+    public ActivityScenarioRule<MainActivity> mActivityScenarioRule =
+            new ActivityScenarioRule<>(MainActivity.class);
+
+    @Test
+    public void mainActivityTest() {
+        ViewInteraction textView = onView(withId(R.id.text_home));
+        textView.check(matches(isDisplayed()));
+        textView.check(matches(withText("This is home fragment")));
+    }
+}
